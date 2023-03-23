1702a5dc7fb035ed81eda90d54b9170fe6084b4d
jordi <89167964+jordi-stack@users.noreply.github.com>
2023-03-23 12:30:03 +0700

    Update monitor.sh

--- wormholeschain/monitor.sh
+++ wormholeschain/monitor.sh
@@ -7,10 +7,10 @@ function info(){
             echo "the monitor version is $vl"
             echo "$cn second."
             echo "node $1"
-            rs1=`curl -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"net_peerCount","id":64}' 127.0.0.1:$1 2>/dev/null`
+            rs1=`curl -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"net_peerCount","id":64}' 127.0.0.1:8101 2>/dev/null`
             count=$(parse_json $rs1 "result")
             echo "Number of node connections: $((16#${count:2}))"
-            rs2=`curl -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","id":64}' 127.0.0.1:$1 2>/dev/null`
+            rs2=`curl -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","id":64}' 127.0.0.1:8101 2>/dev/null`
             blckNumber=$(parse_json $rs2 "result")
             echo "Block height of the current peer: $((16#${blckNumber:2}))"
             sleep 5
@@ -19,33 +19,9 @@ function info(){
    done
 }
 
-function info(){
-    cn=0
-    vl=$(wget https://docker.wormholes.com/version>/dev/null 2>&1 && cat version|awk '{print $2}')
-    while true
-    do
-        echo "the monitor version is $vl"
-        echo "$cn second."
-        echo "node $1"
-        rs1=$(curl -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"net_peerCount","id":64}' 127.0.0.1:$1 2>/dev/null)
-        count=$(parse_json "$rs1" "result")
-        if [[ $count == 0x0 ]]; then
-            sleep 5
-            continue
-        if
-        echo "Number of node connections: $((16#${count:2}))"
-        rs2=$(curl -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","id":64}' 127.0.0.1:$1 2>/dev/null)
-        blckNumber=$(parse_json "$rs2" "result")
-        if [[ $blckNumber == 0x0 ]]; then
-            sleep 5
-            continue
-        fi
-        echo "Block height of the current peer: $((16#${blckNumber:2}))"
-        sleep 5
-        clear
-        let cn+=5
+function parse_json(){
+   echo "${1//\"/}"|sed "s/.*$2:\([^,}]*\).*/\1/"
 }
-
 
 function main(){
    if [[ $# -eq 0 ]];then
